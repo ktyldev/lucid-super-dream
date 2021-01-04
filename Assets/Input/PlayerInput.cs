641f250e3d76b4f736a7d844384d7211fcdef649
Dom Harris <dom@appt.digital>
2021-01-04 15:19:09 +0000

    initial setup

---
+++ Assets/Input/PlayerInput.cs
@@ -0,0 +1,245 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Input
+{
+    public class @PlayerInput : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @PlayerInput()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Default"",
+            ""id"": ""a0990d9b-534a-43cc-aa4a-84f0c8e4e4bc"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""32cfb6a9-3009-425d-8f82-2dfa9698efaa"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Aim"",
+                    ""type"": ""Value"",
+                    ""id"": ""9abef9b2-3340-47b6-b7af-a104b6d41be7"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""28081da1-6270-42ec-8504-ff1f476f33b7"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""fd2f1ebd-5310-4593-9f36-e049c01fb8f3"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""afed6fb1-33d0-478a-9120-72fe065e0cd0"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""742e4c4c-8c85-451c-8823-7fe9ced17043"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""4ffffc9e-de18-4f37-ba11-2a609eb9be8b"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""db23df51-1ee5-4177-8e67-9ceb38f96fd3"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""82329f49-043b-4f3a-859d-bb380174073e"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""17a99488-cfd0-49ac-a921-5e498beabeee"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Aim"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Default"",
+            ""bindingGroup"": ""Default"",
+            ""devices"": []
+        }
+    ]
+}");
+            // Default
+            m_Default = asset.FindActionMap("Default", throwIfNotFound: true);
+            m_Default_Move = m_Default.FindAction("Move", throwIfNotFound: true);
+            m_Default_Aim = m_Default.FindAction("Aim", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Default
+        private readonly InputActionMap m_Default;
+        private IDefaultActions m_DefaultActionsCallbackInterface;
+        private readonly InputAction m_Default_Move;
+        private readonly InputAction m_Default_Aim;
+        public struct DefaultActions
+        {
+            private @PlayerInput m_Wrapper;
+            public DefaultActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Move => m_Wrapper.m_Default_Move;
+            public InputAction @Aim => m_Wrapper.m_Default_Aim;
+            public InputActionMap Get() { return m_Wrapper.m_Default; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(DefaultActions set) { return set.Get(); }
+            public void SetCallbacks(IDefaultActions instance)
+            {
+                if (m_Wrapper.m_DefaultActionsCallbackInterface != null)
+                {
+                    @Move.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMove;
+                    @Move.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMove;
+                    @Move.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMove;
+                    @Aim.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnAim;
+                    @Aim.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnAim;
+                    @Aim.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnAim;
+                }
+                m_Wrapper.m_DefaultActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Move.started += instance.OnMove;
+                    @Move.performed += instance.OnMove;
+                    @Move.canceled += instance.OnMove;
+                    @Aim.started += instance.OnAim;
+                    @Aim.performed += instance.OnAim;
+                    @Aim.canceled += instance.OnAim;
+                }
+            }
+        }
+        public DefaultActions @Default => new DefaultActions(this);
+        private int m_DefaultSchemeIndex = -1;
+        public InputControlScheme DefaultScheme
+        {
+            get
+            {
+                if (m_DefaultSchemeIndex == -1) m_DefaultSchemeIndex = asset.FindControlSchemeIndex("Default");
+                return asset.controlSchemes[m_DefaultSchemeIndex];
+            }
+        }
+        public interface IDefaultActions
+        {
+            void OnMove(InputAction.CallbackContext context);
+            void OnAim(InputAction.CallbackContext context);
+        }
+    }
+}
