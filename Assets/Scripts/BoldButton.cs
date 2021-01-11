672b5c36fdf588e3ec69b85d4809c3e181cd54df
Dom Harris <dom@appt.digital>
2021-01-11 10:01:41 +0000

    menu

---
+++ Assets/Scripts/BoldButton.cs
@@ -0,0 +1,53 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class BoldButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    [SerializeField] private UnityEvent Select;
+    
+    private TextMeshProUGUI _text;
+    private static BoldButton _currentBold;
+    private Tweener _tween;
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void BoldText()
+    {
+         _currentBold?.UnBoldText();
+         _currentBold = this;
+        _text.fontStyle = FontStyles.Bold;
+
+        Select?.Invoke();
+    }
+
+    private void UnBoldText()
+    {
+        _text.fontStyle = FontStyles.Normal;
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        BoldText();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnBoldText();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        BoldText();
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        UnBoldText();
+    }
+}
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,15 +3,24 @@ using System;
 public static class Score
 {
     private static ulong _value;
-    public static ulong Value
+    public static ulong Value => _value;
+
+    public static void Add(ulong val)
     {
-        get => _value;
-        set
-        {
-            _value = value;
-            ScoreUpdated?.Invoke(_value);
-        }
+        _value += val;
+        ScoreChanged();
     }
-    
+
+    public static void Reset()
+    {
+        _value = 0;
+        ScoreChanged();
+    }
+
+    private static void ScoreChanged()
+    {
+        ScoreUpdated?.Invoke(_value);
+    }
+
     public static event Action<ulong> ScoreUpdated;
 }
